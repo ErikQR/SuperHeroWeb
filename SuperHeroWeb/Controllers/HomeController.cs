48df93d1e4dcf17e2eb2dbbdca264dd4fb8e4226
ErikQR <erik.q.retamal@gmail.com>
2022-12-27 15:36:50 -0300

    creacion de bd, se empieza a trabajar en metodos de obtencion de los datos desde la bd, actualizacion de la clase heroe, se añade id en dicha clase, se actualiza vista index

--- SuperHeroWeb/Controllers/HomeController.cs
+++ SuperHeroWeb/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace SuperHeroWeb.Controllers
 {
+
     public class HomeController : Controller
     {
         // GET: Home
--- SuperHeroWeb/Models/DataBase.cs
+++ SuperHeroWeb/Models/DataBase.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,41 @@ namespace SuperHeroWeb.Models
 {
     public class DataBase
     {
+
+        //TODO: Obtencion de heroes desde la bd
+        private List<Heroe> ObtenerHeroes() { 
+            List<Heroe> losHeroes = new List<Heroe>();
+            String cnnStr = ConfigurationManager.ConnectionStrings["cnn"].ConnectionString;
+
+            using (SqlConnection cnn = new SqlConnection(cnnStr)) { 
+                cnn.Open();
+
+                SqlCommand cmdSql = new SqlCommand("Select * from Heroes order by Id", cnn);
+
+                SqlDataReader dr = cmdSql.ExecuteReader();
+
+
+                while (dr.Read()) {
+                    //losHeroes.Add(new Heroe(dr.GetInt32(0), dr["Nombre"].ToString(), dr["Superpoder"].ToString(),dr.GetInt32(0),));
+                }
+            }
+
+                return losHeroes;
+        }
+
+        //TODO: Busqueda de heroes segun id
+        
+        
+        //TODO: creacion de metodo create
+
+
+        //TODO: creacion metodo read
+
+
+        //TODO: creacion metodo update
+
+
+        //TODO: creacion metodo delete
+
     }
 }
